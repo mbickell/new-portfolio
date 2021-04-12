fc607d04e31072a616681f759096d91d70288e9e
Matthew Bickell <matthew@nology.io>
2021-04-12 22:18:44 +0100

    Added first project

--- src/typescript/data/portfolioData.ts
+++ src/typescript/data/portfolioData.ts
@@ -2,16 +2,15 @@ import IPortfolio from "../interfaces/portfolio.interface";
 
 const portfolioData: IPortfolio[] = [
   {
-    title: "Project",
+    title: "mkrc",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptate ipsum perspiciatis. Neque commodiimpedit nam alias. Quos facere nulla iste quisquam excepturi esse natus, quia fugiat, non similiqueminima.",
-    image: "http://www.fillmurray.com/g/400/200",
-    liveLink: "https://www.google.com",
-    githubLink: "https://www.github.com",
+      "An NPM package made to generate components in React. Fueled by jealousy that there was no simple option to quickly create components containing boilerplate like in Angular, I made this save time while creating a new React project. The footprint is small, having only one dependency, and the operation isn't very fancy as it only generates components in a way that I prefer. It has been added to over time and now also supports Functional, Class based and TypeScript React.",
+    image: "./assets/projects/mkrc.jpg",
+    liveLink: "https://www.npmjs.com/package/mkrc",
+    githubLink: "https://github.com/mbickell/mkrc",
     stack: [
-      "html5",
-      "css3",
-      "js"
+      "node",
+      "npm"
     ]
   },
   {
