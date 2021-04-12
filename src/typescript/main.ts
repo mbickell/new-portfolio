3eee6d7f5ceb52d8851df3bacbfa72973949dbcd
Matthew Bickell <matthew@nology.io>
2021-04-12 10:00:51 +0100

    Finished media queries for portfolio

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -11,20 +11,21 @@ const createPortfolioItem = ({ title, description, image, liveLink, githubLink, 
   return `
     <article class="portfolio-wrapper">
       <img src="${image}" alt="${title}" class="portfolio-wrapper__img" />
-
-      <div class="portfolio-wrapper__text">
-        <h3 class="portfolio-wrapper__title">${title}</h3>
-        <p class="portfolio-wrapper__description">
-          ${description}
-        </p>
-        <div class="portfolio-wrapper__links">
-          <a class="portfolio-wrapper__link" href="${liveLink}" target="blank" ><span class="fab fa-github-square" ></span></a>
-          <a class="portfolio-wrapper__link" href="${githubLink}" target="blank" ><span class="fas fa-external-link-square-alt" ></span></a>
+      <div class="portfolio-wrapper__content" />
+        <div class="portfolio-wrapper__text">
+          <h3 class="portfolio-wrapper__title">${title}</h3>
+          <p class="portfolio-wrapper__description">
+            ${description}
+          </p>
+          <div class="portfolio-wrapper__links">
+            <a class="portfolio-wrapper__link" href="${liveLink}" target="blank" ><span class="fab fa-github-square" ></span></a>
+            <a class="portfolio-wrapper__link" href="${githubLink}" target="blank" ><span class="fas fa-external-link-square-alt" ></span></a>
+          </div>
         </div>
-      </div>
 
-      <div class="portfolio-wrapper__stack">
-        ${skills.join("")}
+        <div class="portfolio-wrapper__stack">
+          ${skills.join("")}
+        </div>
       </div>
     </article>
   `;
